0c0162820b642a442d4d42d8af25164fcd08d763
Daniel Sturm <d.sturm@zirkeldesign.de>
2023-11-09 11:56:26 +0100

    :construction: Rename abstract and move filter logic to AcornFSE.php

--- src/AcornFSE.php
+++ src/AcornFSE.php
@@ -2,38 +2,118 @@
 
 namespace Zirkeldesign\AcornFSE;
 
-use Illuminate\Support\Arr;
 use Roots\Acorn\Application;
+use Roots\Acorn\Sage\ViewFinder;
+use function Roots\add_filters;
+use function Roots\remove_filters;
 
 class AcornFSE
 {
-    /**
-     * The application instance.
-     *
-     * @var \Roots\Acorn\Application
-     */
-    protected $app;
+    public array $hierarchy_hooks = [
+        'index_template_hierarchy',
+        '404_template_hierarchy',
+        'archive_template_hierarchy',
+        'author_template_hierarchy',
+        'category_template_hierarchy',
+        'tag_template_hierarchy',
+        'taxonomy_template_hierarchy',
+        'date_template_hierarchy',
+        'home_template_hierarchy',
+        'frontpage_template_hierarchy',
+        'page_template_hierarchy',
+        'paged_template_hierarchy',
+        'search_template_hierarchy',
+        'single_template_hierarchy',
+        'singular_template_hierarchy',
+        'attachment_template_hierarchy',
+        'privacypolicy_template_hierarchy',
+        'embed_template_hierarchy',
+    ];
 
     /**
      * Create a new AcornFSE instance.
      *
-     * @param  \Roots\Acorn\Application  $app
-     * @return void
+     * @param  Application  $app
+     * @param  ViewFinder  $sageFinder
      */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
+    public function __construct(
+        private readonly Application $app,
+        private readonly ViewFinder $sageFinder,
+    ) {
+        add_action('after_setup_theme', [$this, 'addThemeSupport']);
     }
 
     /**
-     * Retrieve a random inspirational quote.
+     * Enable theme support for FSE.
+     *
+     * @return void
+     */
+    public function addThemeSupport(): void
+    {
+        \add_theme_support('block-templates');
+    }
+
+    /**
+     * Use own template filters besides Acorn filters to support FSE.
+     *
+     * @return void
+     */
+    public function bindCompatFilters(): void
+    {
+        $this->removeAcornTemplateFilters();
+
+        add_filters($this->hierarchy_hooks, [$this, 'filterTemplateHierarchy']);
+
+        add_filter('template_include', [$this, 'filterTemplateInclude'], 100);
+    }
+
+    /**
+     * Remove Acorn's template filters to avoid conflicts.
+     */
+    private function removeAcornTemplateFilters(): void
+    {
+        $sage = $this->app['sage'];
+
+        remove_filters($this->hierarchy_hooks, [$sage, 'filterTemplateHierarchy']);
+
+        \remove_filter('template_include', [$sage, 'filterTemplateInclude'], 100);
+    }
+
+    /**
+     * Build template hierarchy with FSE paths on top.
+     *
+     * @param $files
+     *
+     * @return array
+     */
+    public function filterTemplateHierarchy($files): array
+    {
+        $hierarchy = $this->sageFinder->locate($files);
+
+        // Extract all entries, which point to an official FSE path (e.g. templates/...)
+        $fse_paths = array_filter($hierarchy,
+            static fn($file) => str_starts_with($file, 'templates/') || str_contains($file, 'templates/'));
+        $hierarchy = array_diff($hierarchy, $fse_paths);
+
+        // Build hierarchy with original $files and FSE paths on top.
+        return array_merge($files, $fse_paths, $hierarchy);
+    }
+
+    /**
+     * Add short circuit filter to return existing (non-blade) files early.
+     *
+     * @param $file
      *
      * @return string
      */
-    public function getQuote()
+    public function filterTemplateInclude($file)
     {
-        return Arr::random(
-            config('example.quotes')
-        );
+        if (@file_exists($file)
+            && ! str_contains($file, '.blade.php')
+        ) {
+            return $file;
+        }
+
+        return $this->app['sage']?->filterTemplateInclude($file);
     }
 }
--- src/Providers/AcornFSEServiceProvider.php
+++ src/Providers/AcornFSEServiceProvider.php
@@ -9,9 +9,6 @@ use Roots\Acorn\ServiceProvider;
 use Zirkeldesign\AcornFSE\Console\AcornFSECommand;
 use Zirkeldesign\AcornFSE\AcornFSE;
 
-use function Roots\add_filters;
-use function Roots\remove_filters;
-
 class AcornFSEServiceProvider extends ServiceProvider
 {
     /**
@@ -21,9 +18,7 @@ class AcornFSEServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('AcornFSE', function () {
-            return new AcornFSE($this->app);
-        });
+        $this->app->singleton('acorn.fse', AcornFSE::class);
 
         $this->mergeConfigFrom(
             __DIR__.'/../../config/fse.php',
@@ -39,6 +34,14 @@ class AcornFSEServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->bound('sage')) {
+            $this->bindCompatFilters();
+        }
+
+        if ($this->app->bound('blade.compiler')) {
+            $this->registerDirectives();
+        }
+
         $this->publishes(
             [
                 __DIR__.'/../../config/fse.php' => $this->app->configPath('fse.php'),
@@ -48,8 +51,8 @@ class AcornFSEServiceProvider extends ServiceProvider
 
         $this->publishes(
             [
-                __DIR__.'/../../stubs/parts' => $this->app->basePath('parts'),
-                __DIR__.'/../../stubs/patterns' => $this->app->basePath('patterns'),
+                __DIR__.'/../../stubs/parts'     => $this->app->basePath('parts'),
+                __DIR__.'/../../stubs/patterns'  => $this->app->basePath('patterns'),
                 __DIR__.'/../../stubs/templates' => $this->app->basePath('templates'),
             ],
             'fse-theme-files'
@@ -59,86 +62,13 @@ class AcornFSEServiceProvider extends ServiceProvider
             AcornFSECommand::class,
         ]);
 
-        if ($this->app->bound('sage')) {
-            $sage = $this->app['sage'];
-            if (method_exists($sage, 'filterTemplateHierarchy')) {
-                $this->app->getProvider(SageServiceProvider::class)?->booted(fn() => $this->bindCompatFilters());
-            }
-        }
-
-        if ($this->app->bound('blade.compiler')) {
-            $this->registerDirectives();
-        }
-
-        $this->app->make('AcornFSE');
+        $this->app->make('acorn.fse');
     }
 
     protected function bindCompatFilters(): void
     {
-        $sage       = $this->app['sage'];
-        $sageFinder = $this->app['sage.finder'];
-
-        $hooks = [
-            'index_template_hierarchy',
-            '404_template_hierarchy',
-            'archive_template_hierarchy',
-            'author_template_hierarchy',
-            'category_template_hierarchy',
-            'tag_template_hierarchy',
-            'taxonomy_template_hierarchy',
-            'date_template_hierarchy',
-            'home_template_hierarchy',
-            'frontpage_template_hierarchy',
-            'page_template_hierarchy',
-            'paged_template_hierarchy',
-            'search_template_hierarchy',
-            'single_template_hierarchy',
-            'singular_template_hierarchy',
-            'attachment_template_hierarchy',
-            'privacypolicy_template_hierarchy',
-            'embed_template_hierarchy',
-        ];
-
-        remove_filters(
-            $hooks,
-            [$sage, 'filterTemplateHierarchy']
-        );
-
-        add_filters(
-            $hooks,
-            function (array $files) use ($sageFinder) {
-                $hierarchy = $sageFinder->locate($files);
-
-                // Extract all entries, which point to an official FSE path (e.g. templates/...)
-                $fse_paths = array_filter($hierarchy,
-                    static fn($file) => str_starts_with($file, 'templates/') || str_contains($file, 'templates/'));
-                $hierarchy = array_diff($hierarchy, $fse_paths);
-
-                // Build hierarchy with original $files and FSE paths on top.
-                return array_merge($files, $fse_paths, $hierarchy);
-            },
-            10
-        );
-
-        remove_filter(
-            'template_include',
-            [$sage, 'filterTemplateInclude'],
-            100
-        );
-
-        add_filter(
-            'template_include',
-            function ($file) use ($sage) {
-                if (@file_exists($file)
-                    && ! str_contains($file, '.blade.php')
-                ) {
-                    return $file;
-                }
-
-                return $sage->filterTemplateInclude($file);
-            },
-            100
-        );
+        $this->app->getProvider(SageServiceProvider::class)?->booted(fn(
+        ) => $this->app['acorn.fse']->bindCompatFilters());
     }
 
     protected function registerDirectives(): void
