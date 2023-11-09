05ae2533a923e8d8e83b2a0b0c53bdfc416aa2ad
Daniel Sturm <d.sturm@zirkeldesign.de>
2023-11-09 18:24:46 +0100

    Refactor AcornFSEServiceProvider.php

--- src/Providers/AcornFSEServiceProvider.php
+++ src/Providers/AcornFSEServiceProvider.php
@@ -4,10 +4,9 @@ namespace Zirkeldesign\AcornFSE\Providers;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Blade;
-use Roots\Acorn\Sage\SageServiceProvider;
 use Roots\Acorn\ServiceProvider;
-use Zirkeldesign\AcornFSE\Console\AcornFSECommand;
 use Zirkeldesign\AcornFSE\AcornFSE;
+use Zirkeldesign\AcornFSE\Console\AcornFSECommand;
 
 class AcornFSEServiceProvider extends ServiceProvider
 {
@@ -30,12 +29,13 @@ class AcornFSEServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      *
      * @return void
+     *
      * @throws BindingResolutionException
      */
     public function boot()
     {
         if ($this->app->bound('sage')) {
-            $this->bindCompatFilters();
+            $this->app['acorn.fse']->prepareSageTheme();
         }
 
         if ($this->app->bound('blade.compiler')) {
@@ -51,8 +51,8 @@ class AcornFSEServiceProvider extends ServiceProvider
 
         $this->publishes(
             [
-                __DIR__.'/../../stubs/parts'     => $this->app->basePath('parts'),
-                __DIR__.'/../../stubs/patterns'  => $this->app->basePath('patterns'),
+                __DIR__.'/../../stubs/parts' => $this->app->basePath('parts'),
+                __DIR__.'/../../stubs/patterns' => $this->app->basePath('patterns'),
                 __DIR__.'/../../stubs/templates' => $this->app->basePath('templates'),
             ],
             'fse-theme-files'
@@ -65,22 +65,18 @@ class AcornFSEServiceProvider extends ServiceProvider
         $this->app->make('acorn.fse');
     }
 
-    protected function bindCompatFilters(): void
-    {
-        $this->app->getProvider(SageServiceProvider::class)?->booted(fn(
-        ) => $this->app['acorn.fse']->bindCompatFilters());
-    }
-
     protected function registerDirectives(): void
     {
+        Blade::directive('parseblocks', static fn () => "<?php if (\wp_is_block_theme()): ob_start(); ?>");
+        Blade::directive('endparseblocks', static fn () => '<?php $block_content = ob_get_clean(); echo do_blocks($block_content); endif; ?>');
+
         Blade::directive(
-            'blockTemplate',
-            static fn($block) => $block !== ''
+            'blocktemplate',
+            static fn ($block) => $block !== ''
                 ? "<?php if (\wp_is_block_theme()): block_template_part({$block}); else: ?>"
                 : "<?php if (!\wp_is_block_theme()): ?>"
         );
 
-        Blade::directive('endblockTemplate', static fn() => "<?php endif; ?>");
-        Blade::directive('endBlockTemplate', static fn() => "<?php endif; ?>");
+        Blade::directive('endblocktemplate', static fn () => '<?php endif; ?>');
     }
 }
